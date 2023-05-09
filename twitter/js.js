63a5200fa044b6f263b7a0be68b0d67b78b6c65a
britoalana <110117693+britoalana@users.noreply.github.com>
2023-05-08 22:57:12 -0300

    Update js.js

--- twitter/js.js
+++ twitter/js.js
@@ -1,14 +1,14 @@
-var modal = document.getElementById("myModal") // obtém o modal
-var btn = document.getElementById("myBtn") // obtém o botão que abre o modal
-var span = document.getElementsByClassName("close")[0] // obtém o elemento <span> que fecha o modal
+var botaoEntrar = document.querySelector('#myBtn');
+var modal = document.querySelector('#meu-modal');
+var botaoFechar = document.querySelectorAll('.modal-fechar')[0];
 
-// quando o usuário clicar no botão, abre o modal
-btn.onclick = function () {
-    modal.style.display = "block"
+botaoEntrar.onclick = function() {
+  modal.style.display = "block";
 }
 
-// quando o usuário clicar no X, feche o modal
-span.onclick = function () {
-    modal.style.display = "none"
+botaoFechar.onclick = function() {
+  modal.style.display = "none";
 }
+
+
 
