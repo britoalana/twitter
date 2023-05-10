a2f276148d1f13418cd90df19c67a204070a753a
britoalana <110117693+britoalana@users.noreply.github.com>
2023-05-09 22:07:30 -0300

    Update js.js

--- twitter/js.js
+++ twitter/js.js
@@ -10,5 +10,19 @@ botaoFechar.onclick = function() {
   modal.style.display = "none";
 }
 
+var email = document.getElementById('email');
+var form = document.getElementById('form');
+var messageError = document.getElementById('error');
 
+form.addEventListener('submit', (event) => {
 
+  let messages = [];
+  if(email.value === '' || email.value === null) {
+    messages.push('O campo e-mail é obrigatório');
+}
+
+if(messages.length > 0) {
+  event.preventDefault();
+  messageError.innerHTML = messages.join(', ');
+}
+})
